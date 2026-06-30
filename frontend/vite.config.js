374965189084fb19bdf3ded0163d37a7a77a50a4
Suriya A <suriyatryit@gmail.com>
2026-06-30 17:44:01 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-swc' // or '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/gst-invoice-generator/', // <-- MAKE SURE THIS LINE IS PRESENT
 })
